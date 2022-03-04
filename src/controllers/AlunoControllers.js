b6cec47175f5633f94072f0e158c480f2ab5e49d
Mateus Urtassum <mateusurtassum@gmail.com>
2022-03-03 21:59:53 -0300

    Foi criado as rotas para (index, delete, show, update, store) e ainda com loginRequired

--- src/controllers/AlunoControllers.js
+++ src/controllers/AlunoControllers.js
@@ -5,6 +5,94 @@ class AlunoController { // salva
     const alunos = await aluno.findAll();
     res.json(alunos);
   }
+
+  async store(req, res) {
+    try {
+      const alunoStore = await aluno.create(req.body);
+      return res.json(alunoStore);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Missing ID'],
+        });
+      }
+
+      const alunoShow = await aluno.findByPk(id);
+      if (!alunoShow) {
+        return res.status(400).json({
+          errors: ["Aluno doesn't exist"],
+        });
+      }
+
+      return res.json(alunoShow);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Missing ID'],
+        });
+      }
+
+      const alunoShow = await aluno.findByPk(id);
+      if (!alunoShow) {
+        return res.status(400).json({
+          errors: ["Aluno doesn't exist"],
+        });
+      }
+
+      const alunoAtualizado = await alunoShow.update(req.body);
+
+      return res.json(alunoAtualizado);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Missing ID'],
+        });
+      }
+
+      const alunoShow = await aluno.findByPk(id);
+      if (!alunoShow) {
+        return res.status(400).json({
+          errors: ["Aluno doesn't exist"],
+        });
+      }
+
+      await alunoShow.destroy();
+      return res.json({
+        apagado: true,
+      });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 export default new AlunoController();
--- src/models/Aluno.js
+++ src/models/Aluno.js
@@ -5,12 +5,65 @@ export default class aluno extends Model {
   static init(sequelize) {
     super.init(
       {
-        nome: Sequelize.STRING,
-        sobrenome: Sequelize.STRING,
-        email: Sequelize.STRING,
-        idade: Sequelize.INTEGER,
-        peso: Sequelize.FLOAT,
-        altura: Sequelize.FLOAT,
+        nome: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [3, 255],
+              msg: 'Nome precisa ter entre 3 e 255 caracteres',
+            },
+          },
+        },
+        sobrenome: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [3, 255],
+              msg: 'Sobrenome precisa ter entre 3 e 255 caracteres',
+            },
+          },
+        },
+        email: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          unique: {
+            msg: 'Email already exists',
+          },
+          validate: {
+            isEmail: {
+              msg: 'Email inválido',
+            },
+          },
+        },
+        idade: {
+          type: Sequelize.INTEGER,
+          defaultValue: '',
+          validate: {
+            isInt: {
+              msg: 'Idade precisa ser um número inteiro',
+            },
+          },
+        },
+        peso: {
+          type: Sequelize.FLOAT,
+          defaultValue: '',
+          validate: {
+            isFloat: {
+              msg: 'Peso precia ser um número inteiro ou de ponto flutuante',
+            },
+          },
+        },
+        altura: {
+          type: Sequelize.FLOAT,
+          defaultValue: '',
+          validate: {
+            isFloat: {
+              msg: 'Altura precia ser um número inteiro ou de ponto flutuante',
+            },
+          },
+        },
       },
       {
         sequelize,
