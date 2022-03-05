5e179ddcfe87e41f0ece9fabab59616678c63203
Mateus Urtassum <mateusurtassum@gmail.com>
2022-03-05 10:15:43 -0300

    Filtragem do arquivos que são enviado. Funcionalidade incrementada no multerConfig.js

--- src/controllers/FotoControllers.js
+++ src/controllers/FotoControllers.js
@@ -1,7 +1,22 @@
+import multer from 'multer';
+
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig).single('foto');// O SINGLE, É PQ RECEBE APENAS UM ARQUIVO
+/* o multer faz a filtragem do arquivo e o salvamento do texto, reotrna um erro se
+o arquivo for errado */
 class FotoController { // salva
   async store(req, res) {
-    res.json(req.file); // o que recebe do middleware upload.single('foto')
+    return upload(req, res, (error) => {
+      if (error) {
+        return res.status(400).json({
+          errors: [error.code],
+        });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
-
+// o que recebe do middleware upload.single('foto')
 export default new FotoController();
--- src/routes/FotoRoutes.js
+++ src/routes/FotoRoutes.js
@@ -1,12 +1,8 @@
 import { Router } from 'express';
-import multer from 'multer';
 
 import FotoControllers from '../controllers/FotoControllers';
-import multerConfig from '../config/multerConfig';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 // foto pois é o nome criado no insomnia Fotos/POST Store, multipart
-router.post('/', upload.single('foto'), FotoControllers.store);// O SINGLE, É PQ RECEBE APENAS UM ARQUIVO
+router.post('/', FotoControllers.store);
 export default router;
